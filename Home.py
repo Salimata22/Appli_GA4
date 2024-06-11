df703bdc13cc0c5a960c84cf58edc4897ad6ad4c
salimata22 <togo27317@email.com>
2024-06-11 11:08:22 +0200

    apdate

--- Home.py
+++ Home.py
@@ -1,18 +1,6 @@
 import streamlit as st
-from PIL import Image
-import base64
-from io import BytesIO
 
-def get_image_base64(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode()
 def main():
-    # Chemin de l'image du logo
-    logo_path = "logoRetro.png"
-    
-    # Convertir l'image en base64
-    logo_base64 = get_image_base64(logo_path)
-    
     # Ajout de CSS pour les styles et animations
     st.markdown("""
     <style>
@@ -31,12 +19,6 @@ def main():
     h2 {
         color: #ffffff;
     }
-    .logo-container {
-        position: absolute;
-        top: 10px;
-        right: 10px;
-        width: 150px;
-    }
     </style>
     """, unsafe_allow_html=True)
     
@@ -46,9 +28,6 @@ def main():
     ## Description de l'application
     Cette application permet une **analyse approfondie des données GA4** pour Retro Photo, une entreprise spécialisée dans la vente de cartes postales d'anciennes photos. En exploitant les données de leur site web, cette application aide à visualiser et analyser le comportement des utilisateurs, l'engagement sur le site, et les performances des différentes pages.
     """, unsafe_allow_html=True)
-    
-    # Affichage du logo en haut à droite
-    st.markdown(f'<div class="logo-container"><img src="data:image/png;base64,{logo_base64}" alt="Logo"></div>', unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
